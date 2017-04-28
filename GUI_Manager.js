af18a3123906c2265f1f64c428894e33a10c4f18
morga549 <jack.art.morgan@gmail.com>
2017-04-28 11:02:58 -0500

    Revised GUI_Manager.

--- GUI_Manager.js
+++ GUI_Manager.js
@@ -13,11 +13,13 @@
 
         // Physics Text
         this.physText = new createjs.Container();
+        //Background box
         var g = new createjs.Graphics().beginFill("#646464").drawRoundRect(0,0,250, 175, 5);
         var textBox = new createjs.Shape(g);
         textBox.alpha = 0.7;
         textBox.x = 900;
         textBox.y = 170;
+        //Displayed text
         var newText = new createjs.Text("", "30px Arial", "#000000");
         newText.name = "newText";
         newText.x = textBox.x + 10;
@@ -25,35 +27,29 @@
         this.physText.addChild(textBox, newText);
 
         //Pause screen
-        this.pauseScreen = new createjs.Container();
-        // var g1 = new createjs.Graphics().beginFill("#d1d1d1").drawRoundRect(0,0,1180,1100,20);
-        // //Background
-        // var backBox = new createjs.Shape(g1);
-        // backBox.x = backBox.y = 10;
-        // backBox.alpha = 0.7;
-        //Pause Text
-        var pauseText = new createjs.Bitmap("Assets/PauseScreen2.png");
-        pauseText.alpha = 0.8;
-        pauseText.x = 200;
-        pauseText.y = 250;
-
-        this.pauseScreen.addChild(pauseText);
+        this.pauseScreen = new createjs.Bitmap("Assets/PauseScreen2.png");
+        this.pauseScreen.alpha = 0.8;
+        this.pauseScreen.x = 200;
+        this.pauseScreen.y = 250;
         this.pauseScreen.visible = false;
 
         //Fuel Bars
         this.bars = new createjs.Container();
+        //Monopropellant bar
         var monoBar = new window.objects.FuelBar(750, 50, "#FA0A20", "#000000");
         monoBar.name = "monoBar";
         monoBar.setLabel("Monopropellant");
 
+        //Main engine fuel bar
         var fuelBar = new window.objects.FuelBar(750, 110, "#0A6FB4", "#000000");
         fuelBar.name = "fuelBar";
         fuelBar.setLabel("Rocket Fuel");
         this.bars.addChild(monoBar, fuelBar);
 
-
-        this.landedText = new createjs.Text("LANDED!", "70px Impact", "#FF0911");
-        this.landedText.set({x:500, y:500});
+        //Text displayed after successful landing.
+        this.landedText = new createjs.Text("LANDED!", "140px Impact", "#FF0911");
+        this.landedText.set({regX:(225), regY:(100)});
+        this.landedText.set({x:600, y:600});
         this.landedText.visible = false;
 
     };
@@ -65,12 +61,17 @@
 
 
     guim.updatePhysText = function(input){
-       this.physText.getChildByName("newText").text = input;
+        this.physText.getChildByName("newText").text = input;
     };
 
     guim.updateBars = function(monoPercent, fuelPercent) {
         this.bars.getChildByName("monoBar").updateFill(monoPercent);
         this.bars.getChildByName("fuelBar").updateFill(fuelPercent);
+    };
+
+    guim.convertToMeters = function(altitude){
+        const PIXELS_PER_METER = 496 / 52;
+        return altitude / PIXELS_PER_METER;
     };
 
     guim.switchPauseScreen = function(){
@@ -82,7 +83,7 @@
         createjs.Ticker.paused = true;
         this.landedText.visible = true;
 
-        createjs.Tween.get(this.landedText, {ignoreGlobalPause:true} )
+        createjs.Tween.get(this.landedText)
             .to({alpha:.6}, 300)
             .wait(100)
             .to({alpha:1}, 300)

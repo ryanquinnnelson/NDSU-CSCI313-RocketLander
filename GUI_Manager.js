6b4ffa0933415f1c11e5474da08538c4bc93d3da
ryanquinnnelson <ryan.q.nelson@gmail.com>
2017-04-30 13:11:54 -0500

    Comments

--- GUI_Manager.js
+++ GUI_Manager.js
@@ -65,19 +65,21 @@
         var pauseHint = new createjs.Text("Press Spacebar to pause.", "30px Arial", "#000000");
         pauseHint.set({x:810, y:8});
 
+        //Explosion sprite
         this.explosion = new createjs.Sprite(explosion_sheet, "boom");
         this.explosion.set({regX: 32}, {regY:32});
         this.explosion.y = 800;
         this.explosion.set({scaleX: 5, scaleY: 5});
         this.explosion.visible = false;
 
+        //Text displayed after crashed landing.
         this.crashedText = new createjs.Text("CRASHED!", "140px Impact", "#f00911");
         this.crashedText.set({regX:(275), regY:(100)});
         this.crashedText.set({x:600, y:500});
         this.crashedText.visible = false;
 
 
-
+        //add to container
         this.addChild(this.pauseScreen, this.landedText, this.crashedText, this.explosion, this.physBox, this.bars, this.pauseScreen,  pauseHint, this.loadScreen);
 
     };
